646ec4443f2130261b6e88a11302b3e282019bc1
Kim Eunsoo <97098328+kimeunsoo54@users.noreply.github.com>
2024-12-23 21:57:25 +0900

    Update main.py

--- main.py
+++ main.py
@@ -137,7 +137,7 @@ st.divider()
 
 # 4) 파일 업로드 및 목록 표시
 st.subheader("업로드 파일 목록")
-uploaded_file = st.file_uploader("파일을 업로드하세요", accept_multiple_files=True, visibility=False)
+uploaded_file = st.file_uploader("파일을 업로드하세요", accept_multiple_files=True, label_visibility=collpased)
 
 if uploaded_file is not None:
     for i in range(len(UploadedFile)):
